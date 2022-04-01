ec2acb64729322a00172491ee9883f1aba0cccaf
Aleksandr Korostelev <akorostelev@fil-it.ru>
2022-04-01 18:20:56 +0300

    Initial commit

---
+++ src/main/kotlin/creatures/Creature.kt
@@ -0,0 +1,12 @@
+package creatures
+
+class Creature(health: Int, damage: Int, perceptionPoints: Int, agilityPoints: Int) {
+    var health: Int? = 0
+    var damage: Int? = 0
+    var perceptionPoints: Int? = 0
+    var agilityPoints: Int? = 0
+    var experienceBonus: Int? = 0
+    var goldBonus: Int? = 0
+
+constructor()
+}
